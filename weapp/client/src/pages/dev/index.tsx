e2fdd86c9179a8d31d7116c22f5e61b6b129977d
wenxiangdong <wenxiangdong@outlook.com>
2019-07-22 00:06:19 +0800

    init

---
+++ weapp/client/src/pages/dev/index.tsx
@@ -0,0 +1,24 @@
+import {AtButton, AtMessage} from "taro-ui";
+import Taro from '@tarojs/taro';
+import {View} from "@tarojs/components";
+
+function Dev() {
+  const handleClick = () => {
+    Taro.atMessage({
+      message: "hello world",
+      type: "success"
+    })
+  };
+  return (
+    <View>
+      <AtMessage/>
+      <AtButton onClick={handleClick}>hello</AtButton>
+    </View>
+  )
+}
+
+Dev.config = {
+  navigationBarTitleText: "试验"
+};
+
+export default Dev;
