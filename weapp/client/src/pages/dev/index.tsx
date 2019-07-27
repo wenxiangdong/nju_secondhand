262b4cac3e87be9feec3bcca4fad2a11816cdaa4
zlic <161250197@smail.nju.edu.cn>
2019-07-27 17:25:24 +0800

    UPDATE dev 测试页面重置

--- weapp/client/src/pages/dev/index.tsx
+++ weapp/client/src/pages/dev/index.tsx
@@ -1,34 +1,17 @@
 // @ts-ignore
 import Taro from '@tarojs/taro';
 import {View} from "@tarojs/components";
-// import NjuTabBar from "../../components/nju-tab-bar";
 import GoodsCard from "../../components/index/goods-card";
-import {GoodsState, GoodsWithSellerVO, MockGoodsApi} from "../../apis/GoodsApi";
-import {Location, MockUserApi, mockUserApi, UserState} from "../../apis/UserApi";
+import {MockGoodsApi} from "../../apis/GoodsApi";
 import './index.scss'
-import {apiHub} from "../../apis/ApiHub";
 
 function Dev() {
   // noinspection JSIgnoredPromiseFromCall
   Taro.setNavigationBarTitle({title: '测试'});
 
-  const handleClick = () => {
-
-  };
   return (
     <View>
-      {new Array(6).fill('').map((e, idx) => <GoodsCard key={`goodsCard-${idx}`} goodsWithSeller={MockGoodsApi.createMockGoodsWithSeller()}/>)}
-      {/*{*/}
-        {/*Array(30)*/}
-          {/*.fill(0)*/}
-          {/*.map((_, index) => (*/}
-            {/*<View  key={index}  style={{marginTop: "10px"}}>*/}
-              {/*<AtButton onClick={handleClick}>hello</AtButton>*/}
-            {/*</View>*/}
-          {/*))*/}
-      {/*}*/}
-      {/*<WhiteSpace height={50}/>*/}
-      {/*/!*<NjuTabBar/>*!/*/}
+      {new Array(6).fill('').map((e, idx) => <GoodsCard key={`goodsCard-${e}-${idx}`} goodsWithSeller={MockGoodsApi.createMockGoodsWithSeller()}/>)}
     </View>
   )
 }
@@ -37,4 +20,5 @@ Dev.config = {
   navigationBarTitleText: "试验"
 };
 
+// @ts-ignore
 export default Dev;
